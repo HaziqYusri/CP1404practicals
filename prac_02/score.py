fe31ec86fb3a61f6ae90c00eea0d1cc7b8142676
HaziqYusri <01haziqyusri@gmail.com>
2025-02-09 22:21:01 +0800

    Refactored score.py

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,9 +1,18 @@
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    score = float(input("Enter score: "))
+    print(score_rating(score))
+
+
+def score_rating(score):
+    """Determine the status of a given score."""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
