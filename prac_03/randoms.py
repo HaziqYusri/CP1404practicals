0de07c1addb5ae746dda1a28524fe56c98900c22
HaziqYusri <01haziqyusri@gmail.com>
2025-02-16 21:49:59 +0800

    randoms.py

---
+++ prac_03/randoms.py
@@ -0,0 +1,20 @@
+#18
+#5
+#3.395189493931726
+
+#TODO: What did you see on line 1?
+#The smallest possible number: 5
+#The largest possible number: 20
+
+#TODO: What did you see on line 2?
+#The smallest possible number: 3
+#The largest possible number: 9
+#Could line 2 have produced a 4? NO, only odd numbers between 3 to 9 are valid
+
+#TODO: What did you see on line 3?
+#The smallest possible number: 2.5
+#The largest possible number: 5.5
+
+import random
+print(random.randint(1, 100))
+
