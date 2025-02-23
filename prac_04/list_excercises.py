fd3a051bdd06decf070b708fb1ea4015643f9b7a
HaziqYusri <01haziqyusri@gmail.com>
2025-02-23 21:33:09 +0800

    list_excercises.py (with username)

--- prac_04/list_excercises.py
+++ prac_04/list_excercises.py
@@ -1,4 +1,5 @@
 def main():
+    check_username()
     numbers = []  #empty list
 
     for i in range(5): #get 5 numbers
@@ -10,6 +11,22 @@ def main():
     print(f"The last number is {numbers[-1]}")
     print(f"The smallest number is {min(numbers)}")
     print(f"The largest number is {max(numbers)}")
-    print(f"The average of the numbers is {sum(numbers) / len(numbers):.1f}")  # Rounded to 1 decimal place
+    print(f"The average of the numbers is {sum(numbers) / len(numbers):.1f}")
+
+
+
+def check_username():
+    """Check if the user is in the authorized list of usernames."""
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45',
+                 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState',
+                 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+    username = input("\nEnter your username: ")
+
+    if username in usernames:
+        print("Access granted")
+    else:
+        print("Access denied")
+
 
 main()
