85e95332a03a7a570f525af344bfae2dbf50ab76
HaziqYusri <01haziqyusri@gmail.com>
2025-02-23 21:25:16 +0800

    subject_reader.py

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -2,7 +2,7 @@
 CP1404/CP5632 Practical
 Data file -> lists program
 """
-
+"""
 FILENAME = "subject_data.txt"
 
 
@@ -12,7 +12,7 @@ def main():
 
 
 def load_data():
-    """Read data from file formatted like: subject,lecturer,number of students."""
+    #Read data from file formatted like: subject,lecturer,number of students.
     input_file = open(FILENAME)
     for line in input_file:
         print(line)  # See what a line looks like
@@ -25,5 +25,35 @@ def load_data():
         print("----------")
     input_file.close()
 
+
+main()
+"""
+
+FILENAME = "subject_data.txt"
+
+
+def main():
+    subjects = load_data()  # Load subjects into a list of lists
+    subject_details(subjects)
+
+
+def load_data():
+    subjects = []  #initialise list
+
+    with open(FILENAME, "r") as input_file:
+        for line in input_file:
+            line = line.strip()
+            parts = line.split(',')
+            parts[2] = int(parts[2])  #Student count to an integer
+            subjects.append(parts)
+
+    return subjects  # Return list of lists
+
+
+def subject_details(subjects):
+
+    for subject in subjects:
+        subject_code, lecturer, student_count = subject
+        print(f"{subject_code} is taught by {lecturer} and has {student_count} students")
 
 main()
