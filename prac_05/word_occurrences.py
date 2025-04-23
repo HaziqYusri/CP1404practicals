1735e77120b3ffbf683dceb572bd00e2bd91e094
HaziqYusri <01haziqyusri@gmail.com>
2025-04-24 02:55:10 +0800

    word_occurrences.py completed code output and sorting

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -14,4 +14,13 @@ for word in words:
     word = word.strip('.,!?;:"\'')  # Remove punctuation
     word_to_count[word] = word_to_count.get(word, 0) + 1
 
+print(f"\nText: {text}\n")
 
+# Word sorting
+unique_words = list(word_to_count.keys())
+unique_words.sort()
+
+max_word_length = max(len(word) for word in unique_words)
+
+for word in unique_words:
+    print(f"{word:{max_word_length}} : {word_to_count[word]}")
