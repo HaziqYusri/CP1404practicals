2d5476f357b9628841c59a00d4015597edcf8419
HaziqYusri <01haziqyusri@gmail.com>
2025-04-24 02:18:59 +0800

    hex_colours.py completed the dictionary and displaying of it

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,17 @@
+#Dictionary of 10 colours
+HEX_COLOURS = {
+    "aliceblue": "#f0f8ff",
+    "antiquewhite": "#faebd7",
+    "aqua": "#00ffff",
+    "aquamarine": "#7fffd4",
+    "azure": "#f0ffff",
+    "beige": "#f5f5dc",
+    "bisque": "#ffe4c4",
+    "black": "#000000",
+    "blanchedalmond": "#ffebcd",
+    "blue": "#0000ff"
+}
+
+# Display all available color names
+for name, code in HEX_COLOURS.items():
+    print(f"{name.capitalize():<20} is {code}")
