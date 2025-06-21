a7e09bab5c6019c875d58e2f80bd6664ca0f55da
HaziqYusri <01haziqyusri@gmail.com>
2025-06-22 00:43:32 +0800

    Added user input loop

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -20,3 +20,17 @@ HEX_COLOURS = {
 # Display all available color names
 for name, code in HEX_COLOURS.items():
     print(f"{name.capitalize():<20} is {code}")
+
+#User input loop
+colour_name = input("Enter a colour name: ").lower()
+
+while colour_name != "":
+    hex_code = HEX_COLOURS.get(colour_name)
+    if hex_code:
+        print(f"The code for \"{colour_name}\" is {HEX_COLOURS.get(colour_name)}")
+        colour_name = input("Enter a colour name: ").lower()
+    else:
+        print("Invalid colour, please try again.")
+        colour_name = input("Enter a colour name: ").lower()
+
+print("Goodbye!")
