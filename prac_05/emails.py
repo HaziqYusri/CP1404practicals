16a76ee3124add12dbec2d83945c1f92e77f2037
HaziqYusri <01haziqyusri@gmail.com>
2025-06-22 01:03:06 +0800

    Added main code to process names and guess names from the email

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -9,3 +9,29 @@ def extract_name(email):
     parts = email.split('@')[0].split('.')
     name = ' '.join(part.title() for part in parts)
     return name
+
+#Main code to collect and process names
+def main():
+    email_to_name = {}
+
+
+    while True:
+        email = input("Email: ").strip()
+        if email == "":
+            break
+
+        #Guess a name from the email
+        name_guess = extract_name(email)
+        confirmation = input(f"Is your name {name_guess}? (Y/n) ").strip().lower()
+
+        if confirmation not in ("", "y"):
+            name_guess = input("Name: ").strip()
+
+        email_to_name[email] = name_guess
+
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
+
+
+if __name__ == "__main__":
+    main()
