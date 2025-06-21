e0127a122e1451002837fe2dd88e2ed97691765e
HaziqYusri <01haziqyusri@gmail.com>
2025-06-22 00:33:34 +0800

    Creation of hex_colours.py and added dictionary of 10 colours

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,18 @@
+"""
+CP1404 Practical 5
+Hexadecimal colour lookup
+"""
+
+# Dictionary of 10 colours
+HEX_COLOURS = {
+    "aliceblue": "#f0f8ff",
+    "antiquewhite": "#faebd7",
+    "aqua": "#00ffff",
+    "aquamarine": "#7fffd4",
+    "azure": "#f0ffff",
+    "beige": "#f5f5dc",
+    "bisque": "#ffe4c4",
+    "black": "#000000",
+    "blanchedalmond": "#ffebcd",
+    "blue": "#0000ff"
+}
