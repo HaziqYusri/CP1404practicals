c16a76f57b9b6bf4e084c3fc0b8d1501f6b0b5d6
HaziqYusri <01haziqyusri@gmail.com>
2025-06-22 00:49:48 +0800

    Added method to count word occurance

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -8,3 +8,12 @@ Actual:    minutes
 text = input("Enter text: ")
 words = text.lower().split()
 word_to_count = {}
+
+# Count word occurrences
+for word in words:
+    word = word.strip('.,!?;:"\'')  # Remove punctuation
+    word_to_count[word] = word_to_count.get(word, 0) + 1
+
+# Displays text to analyse word occurence
+print(f"\nText: {text}\n")
+
