ed5ae2ebedb2cddb0e3e37fea6eb8aa2471d221c
HaziqYusri <01haziqyusri@gmail.com>
2025-07-06 23:57:32 +0800

    Initialised languages.py per instructions

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -4,3 +4,13 @@ Estimate: 20 minutes
 Actual:    minutes
 """
 
+from programming_language import ProgrammingLanguage
+
+def main():
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    print(python)
+
+main()
