b352a8b300ed485ccc7e1d57bc7be6610bad28cd
HaziqYusri <01haziqyusri@gmail.com>
2025-07-07 00:11:47 +0800

    Added guitar class with follow functions: ___init___(), ___str___(). get_age(), is_vintage()

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -4,8 +4,24 @@ Estimate: 20 minutes
 Actual:    minutes
 """
 
+class Guitar:
+    """Represents the guitar."""
 
-name = "Gibson L-5 CES"
-year = 1922
-cost = 16035.40
-print(f"My guitar: {name}, first made in {year}")
+    def __init__(self, name="", year=0, cost=0):
+        """Initialising guitar"""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Return string representation of the guitar"""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        """Return how old the guitar is in years."""
+        CURRENT_YEAR = 2022
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        """Return True if the guitar is 50 or more years old."""
+        return self.get_age() >= 50
