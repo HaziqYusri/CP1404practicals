32680e86fddbaabeea8ebe9315410d5bc0b566e9
HaziqYusri <01haziqyusri@gmail.com>
2025-07-07 00:26:43 +0800

    Efficient test implemented and formatting improved

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -7,6 +7,7 @@ def main():
     guitars = []
 
     #Manual input version
+    """
     while True:
         name = input("Name: ")
         if not name:
@@ -16,11 +17,16 @@ def main():
         guitar = Guitar(name, year, cost)
         guitars.append(guitar)
         print(f"{guitar} added.\n")
+    """
 
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+    guitars.append(Guitar("Fender Stratocaster", 2014, 765.4))
 
     print("\nThese are my guitars:")
     for i, guitar in enumerate(guitars, 1):
         vintage_string = " (vintage)" if guitar.is_vintage() else ""
-        print(f"Guitar {i}: {guitar.name:>10} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+        print(f"Guitar {i}: {guitar.name:<20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
 
 main()
