2b235bd60f52b6525323a14bf8b429351f18ac4a
HaziqYusri <01haziqyusri@gmail.com>
2025-07-14 02:20:16 +0800

    Initialised project.py and project_management.py to begin work

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -3,4 +3,20 @@ Project management
 Estimate: 30 minutes
 Actual:    minutes
 """
+# TODO: Functions:
+#   load_projects
+#   save_projects
+#   display_projects
+#   filter_projects
+#   add_project
+#   update_project
+
+from datetime import datetime
+from project import Project
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
 
