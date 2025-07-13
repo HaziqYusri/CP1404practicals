be9a115072899cce450fa0073da6f75a1cfb1031
HaziqYusri <01haziqyusri@gmail.com>
2025-07-14 01:34:51 +0800

    Updated main() function to prompt for new guitar (used prev code)

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -13,6 +13,19 @@ def main():
         vintage_string = " (vintage)" if guitar.is_vintage() else ""
         print(f"Guitar {i}: {guitar.name:<20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
 
+    #Lets user add new guitars
+    print("\nAdd new guitars:")
+
+    while True:
+        name = input("Name: ")
+        if not name:
+            break
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        print(f"{guitar} added.\n")
+
 def load_guitars(filename):
     """Read guitars from the CSV and returns a list"""
     guitars = []
