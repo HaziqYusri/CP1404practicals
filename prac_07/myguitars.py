366aab56500f7b605a6e3227d982e649e1d44d1e
HaziqYusri <01haziqyusri@gmail.com>
2025-07-14 01:26:32 +0800

    Added load_guitars() function and integrated guitars.sort() to main()

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -1,32 +1,26 @@
+import csv
 from guitar import Guitar
 
 def main():
-    """Get guitars from user and display them in formatted list."""
+    """Read guitars from CSV, sorts by year then displays them."""
     print("My guitars!")
+    guitars = load_guitars("guitars.csv")
 
-    guitars = []
+    guitars.sort() #Uses __lt__
 
-    #Manual input version
-    """
-    while True:
-        name = input("Name: ")
-        if not name:
-            break
-        year = int(input("Year: "))
-        cost = float(input("Cost: "))
-        guitar = Guitar(name, year, cost)
-        guitars.append(guitar)
-        print(f"{guitar} added.\n")
-    """
-
-    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
-    guitars.append(Guitar("Fender Stratocaster", 2014, 765.4))
-
-    print("\nThese are my guitars:")
+    print("\nThese are my guitars (sorted by year):")
     for i, guitar in enumerate(guitars, 1):
         vintage_string = " (vintage)" if guitar.is_vintage() else ""
         print(f"Guitar {i}: {guitar.name:<20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
 
+def load_guitars(filename):
+    """Read guitars from the CSV and returns a list"""
+    guitars = []
+    with open(filename, 'r') as in_file:
+        reader = csv.reader(in_file)
+        for row in reader:
+            name, year, cost = row
+            guitars.append(Guitar(name, int(year), float(cost)))
+    return guitars
 
 main()
