00f30197cbcf2830a96026165dacd27489081def
HaziqYusri <01haziqyusri@gmail.com>
2025-07-14 02:27:53 +0800

    Updated Project class with __init__() and added working load_projects() function

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,4 +1,11 @@
 from datetime import date
 
 class Project:
-    def __init__(self):
+    """Class to represent a project."""
+    def __init__(self, name, start_date, priority, cost, completion):
+        self.name = name
+        self.start_date = start_date  # datetime.date
+        self.priority = priority
+        self.cost = cost
+        self.completion = completion
+
--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -14,8 +14,34 @@ Actual:    minutes
 from datetime import datetime
 from project import Project
 
-def main():
+FILENAME = "projects.txt"
 
+MENU = """
+- (L)oad projects  
+- (S)ave projects  
+- (D)isplay projects  
+- (F)ilter projects by date
+- (A)dd new project  
+- (U)pdate project
+- (Q)uit
+"""
+
+def main():
+    print("Welcome to Pythonic Project Management")
+    projects = load_projects(FILENAME)
+    print(f"Loaded {len(projects)} projects from {FILENAME}")
+
+def load_projects(filename):
+    """Load projects from file and returns a list"""
+    projects = []
+    with open(filename, 'r') as file:
+        next(file)  # Skip header
+        for line in file:
+            parts = line.strip().split('\t')
+            name, start_str, priority, cost, completion = parts
+            start_date = datetime.strptime(start_str, "%d/%m/%Y").date()
+            projects.append(Project(name, start_date, int(priority), float(cost), int(completion)))
+    return projects
 
 if __name__ == '__main__':
     main()
