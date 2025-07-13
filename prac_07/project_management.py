25e4e946c2802502cdeeab77cd6de667048d8520
HaziqYusri <01haziqyusri@gmail.com>
2025-07-14 04:02:46 +0800

    Completed significant error handling and double checked outputs

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,15 +1,15 @@
 """
 Project management
 Estimate: 30 minutes
-Actual:    minutes
+Actual:   182 minutes
 """
 # TODO: Functions:
-#   load_projects (ongoing)
-#   save_projects (ongoing)
-#   display_projects (ongoing)
-#   filter_projects (ongoing)
-#   add_project (ongoing)
-#   update_project (incomplete)
+#   load_projects (complete)
+#   save_projects (complete)
+#   display_projects (complete)
+#   filter_projects (complete)
+#   add_project (complete)
+#   update_project (complete)
 
 from datetime import datetime
 from project import Project
@@ -35,7 +35,10 @@ def main():
         choice = input(">>> ").lower()
         if choice == 'l':
             filename = input("Enter filename to load: ") + ".txt"
-            projects = load_projects(filename)
+            try:
+                projects = load_projects(filename)
+            except (FileNotFoundError, ValueError):  #Error handling for invalid file
+                print(f"Failed to load from {filename} check the file and try again.")
         elif choice == 's':
             filename = input(f"Enter filename to save:") + ".txt"
             save_projects(filename, projects)
@@ -51,9 +54,12 @@ def main():
             confirm = input(f"Would you like to save to {FILENAME}? ").lower()
             if confirm in ['yes', 'y']:
                 save_projects(FILENAME, projects)
+
+            """ # Good alternative doesn't match sample
             else:
                 filename = input(f"Enter filename to save:") + ".txt"
                 save_projects(filename,projects)
+            """
             print("Thank you for using custom-built project management software.")
             break
         else:
@@ -94,9 +100,12 @@ def display_projects(projects):
 def filter_projects(projects):
     """Filter projects after specified date"""
     date_str = input("Show projects that start after date (dd/mm/yy): ")
-    date = datetime.strptime(date_str, "%d/%m/%y").date()
-    for p in sorted([p for p in projects if p.start_date >= date], key=lambda x: x.start_date):
-        print(p)
+    try:
+        date = datetime.strptime(date_str, "%d/%m/%y").date()
+        for p in sorted([p for p in projects if p.start_date >= date], key=lambda x: x.start_date):
+            print(p)
+    except ValueError:
+        print("Invalid date format. Please enter as dd/mm/yy.")  # Added error handling
 
 def add_project(projects):
     """Adds new project to projects list"""
@@ -113,15 +122,21 @@ def update_project(projects):
     """Updates existing project in projects list"""
     for i, project in enumerate(projects):
         print(f"{i} {project}")
-    index = int(input("Project choice: "))
-    project = projects[index]
-    print(project)
-    new_completion = input("New Percentage: ")
-    if new_completion:
-        project.completion = int(new_completion)
-    new_priority = input("New Priority: ")
-    if new_priority:
-        project.priority = int(new_priority)
+    try:
+        index = int(input("Project choice: "))
+        if not 0 <= index < len(projects):
+            print("Invalid project index.")
+            return
+        project = projects[index]
+        print(project)
+        new_completion = input("New Percentage: ")
+        if new_completion:
+            project.completion = int(new_completion)
+        new_priority = input("New Priority: ")
+        if new_priority:
+            project.priority = int(new_priority)
+    except ValueError:
+        print("Invalid input. Enter valid option.")
 
 if __name__ == '__main__':
     main()
