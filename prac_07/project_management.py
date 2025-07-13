620fd282047857caaa0d82743a75a1e9be202b82
HaziqYusri <01haziqyusri@gmail.com>
2025-07-14 02:34:14 +0800

    Added working menu to main() function

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -4,12 +4,12 @@ Estimate: 30 minutes
 Actual:    minutes
 """
 # TODO: Functions:
-#   load_projects
-#   save_projects
-#   display_projects
-#   filter_projects
-#   add_project
-#   update_project
+#   load_projects (ongoing)
+#   save_projects (incomplete)
+#   display_projects (incomplete)
+#   filter_projects (incomplete)
+#   add_project (incomplete)
+#   update_project (incomplete)
 
 from datetime import datetime
 from project import Project
@@ -31,6 +31,32 @@ def main():
     projects = load_projects(FILENAME)
     print(f"Loaded {len(projects)} projects from {FILENAME}")
 
+    while True:
+        print(MENU)
+        choice = input(">>> ").lower()
+        if choice == 'l':
+            break
+        elif choice == 's':
+            #save_projects(filename, projects)
+            break
+        elif choice == 'd':
+            #display_projects(projects)
+            break
+        elif choice == 'f':
+            #filter_projects(projects)
+            break
+        elif choice == 'a':
+            #add_project(projects)
+            break
+        elif choice == 'u':
+            #update_project(projects)
+            break
+        elif choice == 'q':
+            print("Thank you for using custom-built project management software.")
+            break
+        else:
+            print("Invalid option")
+
 def load_projects(filename):
     """Load projects from file and returns a list"""
     projects = []
@@ -42,6 +68,8 @@ def load_projects(filename):
             start_date = datetime.strptime(start_str, "%d/%m/%Y").date()
             projects.append(Project(name, start_date, int(priority), float(cost), int(completion)))
     return projects
+
+
 
 if __name__ == '__main__':
     main()
